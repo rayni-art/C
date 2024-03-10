80ed5f35fb0a71a0f1e6cbe52cc3714629030809
Rayni Winka <122493914+rayni-art@users.noreply.github.com>
2024-03-09 17:55:44 -0800

    Add files via upload

---
+++ hw6.cpp
@@ -0,0 +1,23 @@
+//Class: CS/IS 135-71LEC (1221)
+//2022 Spring Online class
+//Computer Information System
+//  homework#1
+//  Created by rayni on 3/6/22.
+
+    
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    char asc;
+    cout<<"Please enter a character: ";
+    cin>> asc;
+    cout<<endl;
+    cout <<"The ASCII code for the character "<<char(asc)<<" is "<<int(asc)<<endl;
+    
+    //System("PAUSE")
+    return 0;
+}
