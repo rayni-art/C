9f005b36417c08a2c93340ec9fde6707d1d4472a
Rayni Winka <122493914+rayni-art@users.noreply.github.com>
2024-03-09 18:02:18 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  assign#2
+//
+//  Created by rayni on 2/27/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double s;
+           s =  5;
+           cout << s << endl;
+           return 0;
+}
