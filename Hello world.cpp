e96c6f02545e8e8c0196b78d764244caff1d15ce
Rayni Winka <122493914+rayni-art@users.noreply.github.com>
2024-03-09 18:00:46 -0800

    Add files via upload

---
+++ Hello world.cpp
@@ -0,0 +1,17 @@
+//RuiHe
+//Class: CS/IS 135-71LEC (1221)
+//2022 Spring Online class
+//Thursday Night 5:05pm-6:30pm
+//Computer Information System
+//  main.cpp
+//  assignment#2
+//  Created by rayni on 3/2/22.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
