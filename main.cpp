9c5b6410186fea5951aad38a6ea752956c65fb98
Rui Winka <122493914+rayni-art@users.noreply.github.com>
2024-03-09 16:06:53 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//RuiHe
+//RAYNI He
 //Class: CS/IS 135-71LEC (1221)
 //2022 Spring Online class
 //Thursday Night 5:05pm-6:30pm
@@ -14,7 +14,7 @@ int main()
 {
     cout<<"STUDENT INFORMATION"<<endl;
     cout<<"------------"<<endl;
-    cout<<"NAME: RuiHe"<<endl;
+    cout<<"NAME: Rayni He"<<endl;
     cout<<"COMPUTER LANGUAGES: Java, Python"<<endl;
     cout<<"FAVORITE VIDEO GAME: It takes two"<<endl;
     cout<<"I'm taking this class because I want to understand C++ languages, I think the logic it is import in computer science"<<endl;
