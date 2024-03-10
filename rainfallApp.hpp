f1bf5f65d707f180fb9877b6d615d0d981770c3a
Rayni Winka <122493914+rayni-art@users.noreply.github.com>
2024-03-09 17:40:14 -0800

    Add files via upload

---
+++ rainfallApp.hpp
@@ -0,0 +1,13 @@
+//
+//  rainfallApp.hpp
+//  HomeWork 7 part 2
+//
+//  Created by rayni on 6/4/22.
+//
+
+#ifndef rainfallApp_hpp
+#define rainfallApp_hpp
+
+#include <stdio.h>
+
+#endif /* rainfallApp_hpp */
