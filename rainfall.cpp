2edcb2aa6fef7265b736779b8e3eb5594fe6911e
Rayni Winka <122493914+rayni-art@users.noreply.github.com>
2024-03-09 17:39:06 -0800

    Add files via upload

---
+++ rainfall.cpp
@@ -0,0 +1,23 @@
+//Class: CS/IS 135-71LEC (1221)
+//2022 Spring Online class
+//Computer Information System
+//  Homework7
+//  Created by rayni on 5/27/22.
+//
+
+
+#include "Stats_h.h"
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+Stats::Stats(string M, double)
+
+{
+    const int MONTHS = 12;
+    string months[MONTHS] = { "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" };
+   
+    
+}
